712973a254282600a70732435a0cb0e450d5066a
codeferal <codeferal@protonmail.com>
2024-01-22 19:07:57 +0200

    keymaps and options

--- lua/codeferal/core/keymaps.lua
+++ lua/codeferal/core/keymaps.lua
@@ -1,3 +1,14 @@
+-- set leader key to space
 vim.g.mapleader = " "
 
--- vim.keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
+-- window management
+vim.keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
+vim.keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
+vim.keymap.set("n", "<leader>se", "<C-w>=", { desc = "Makes splits equal size" })
+vim.keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" })
+
+vim.keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })
+vim.keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" })
+vim.keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })
+vim.keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" })
+vim.keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" })
--- lua/codeferal/core/options.lua
+++ lua/codeferal/core/options.lua
@@ -1,2 +1,26 @@
+-- clipboard
+vim.opt.clipboard:append("unnamedplus") -- use system clipboard as default register
+
+-- turn off swapfile
+vim.opt.swapfile = false
+
+-- line numbers
 vim.opt.relativenumber = true -- show relative line numbers
 vim.opt.number = true -- show absolute line number on cursor (when relative number is on)
+
+-- tabs & identation
+vim.opt.tabstop = 2 -- 2 spaces for tabs
+vim.opt.shiftwidth = 2 -- 2 spaces for indent width
+vim.opt.expandtab = true -- expand tab to spaces
+vim.opt.autoindent = true -- copy indent from current line when starting new one
+
+-- line wrapping
+vim.opt.wrap = false
+
+-- search settings
+vim.opt.ignorecase = true -- ignore case when searching
+vim.opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
+
+-- split windows
+vim.opt.splitright = true -- split vertical window to the right
+vim.opt.splitbelow = true -- split horizontal window to the bottom
