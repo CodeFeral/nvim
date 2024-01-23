79de725a5beaa71d2ea4b4e1a8cb4540512911d1
codeferal <codeferal@protonmail.com>
2024-01-23 03:56:15 +0200

    checkhealth hide optionals

--- lua/codeferal/core/options.lua
+++ lua/codeferal/core/options.lua
@@ -1,3 +1,9 @@
+-- checkhealth
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_node_provider = 0
+
 -- clipboard
 vim.opt.clipboard:append("unnamedplus") -- use system clipboard as default register
 
