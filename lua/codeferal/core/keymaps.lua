7c24b84fd57fe6bfbdfae5c1861700d2f9b0cec9
codeferal <codeferal@protonmail.com>
2024-01-23 06:57:16 +0200

    keymap update

--- lua/codeferal/core/keymaps.lua
+++ lua/codeferal/core/keymaps.lua
@@ -2,27 +2,31 @@
 vim.g.mapleader = " "
 
 -- use jk to exit insert mode
-vim.keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
-vim.keymap.set("i", "kj", "<ESC>", { desc = "Exit insert mode with kj" })
+vim.keymap.set("i", "jk", "<ESC>", { desc = "exit insert mode with jk" })
+vim.keymap.set("i", "kj", "<ESC>", { desc = "exit insert mode with kj" })
 
 -- splits
-vim.keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window Vertically" })
-vim.keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window Horizontally" })
-vim.keymap.set("n", "<leader>se", "<C-w>=", { desc = "Splits Equal size" })
-vim.keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "close current Split" })
+vim.keymap.set("n", "<leader>ss", "<C-w>s", { desc = "split window horizontally" })
+vim.keymap.set("n", "<leader>sv", "<C-w>v", { desc = "split window vertically" })
+vim.keymap.set("n", "<leader>se", "<C-w>=", { desc = "splits equal size" })
+vim.keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "close current split" })
+vim.keymap.set("n", "<leader>sh", "<C-w>h", { desc = "go to left split " })
+vim.keymap.set("n", "<leader>sj", "<C-w>j", { desc = "go to down split" })
+vim.keymap.set("n", "<leader>sk", "<C-w>k", { desc = "go to up split" })
+vim.keymap.set("n", "<leader>sl", "<C-w>l", { desc = "go to right split" })
 
 -- tabs
-vim.keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new Tab" })
-vim.keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current Tab" })
-vim.keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "go to Next Tab" })
-vim.keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "go to Previous Tab" })
-vim.keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "open current buffer in new Tab" })
+vim.keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "open new tab" })
+vim.keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "close current tab" })
+vim.keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "go to previous tab" })
+vim.keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "go to next tab" })
+vim.keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "open current buffer in new tab" })
 
 -- Lazy
 vim.keymap.set("n", "<leader>L", "<cmd>Lazy<CR>", { desc = "open Lazy" })
 
 -- Neo-tree
-vim.keymap.set("n", "<leader>fe", "<cmd>Neotree focus filesystem left reveal<CR>", { desc = "Neotree open" })
-vim.keymap.set("n", "<leader>ff", "<cmd>Neotree toggle float reveal<CR>", { desc = "Neotree Float open" })
-vim.keymap.set("n", "<leader>fx", "<cmd>Neotree close<CR>", { desc = "Neotree close" })
-vim.keymap.set("n", "<leader>fg", "<cmd>Neotree git_status toggle float<CR>", { desc = "Neotree Git status" })
+vim.keymap.set("n", "<leader>fe", "<cmd>Neotree focus filesystem left reveal<CR>", { desc = "neotree open" })
+vim.keymap.set("n", "<leader>ff", "<cmd>Neotree toggle float reveal<CR>", { desc = "neotree float open" })
+vim.keymap.set("n", "<leader>fx", "<cmd>Neotree close<CR>", { desc = "neotree close" })
+vim.keymap.set("n", "<leader>fg", "<cmd>Neotree git_status toggle float<CR>", { desc = "neotree git status" })
