9efd12d457081ea93cda44b766a60c621c160b49
codeferal <codeferal@protonmail.com>
2024-01-26 20:12:50 +0200

    fix

--- lua/codeferal/plugins/comment.lua
+++ lua/codeferal/plugins/comment.lua
@@ -1,9 +1,9 @@
--- return {
---   "numToStr/Comment.nvim",
---
---   lazy = false,
---
---   config = function()
---     require("Comment").setup()
---   end,
--- }
+return {
+	"numToStr/Comment.nvim",
+
+	lazy = false,
+
+	config = function()
+		require("Comment").setup()
+	end,
+}
