ba9f9dedf5a2eeb1966d86390f2261dac8025b40
codeferal <codeferal@protonmail.com>
2024-01-26 16:46:49 +0200

    keymaps update

--- lua/codeferal/core/keymaps.lua
+++ lua/codeferal/core/keymaps.lua
@@ -31,11 +31,10 @@ vim.keymap.set({ "n", "v" }, "<leader>ll", vim.lsp.buf.hover, { desc = "look" })
 vim.keymap.set({ "n", "v" }, "<leader>ld", vim.lsp.buf.definition, { desc = "definition" })
 vim.keymap.set({ "n", "v" }, "<leader>lD", vim.lsp.buf.declaration, { desc = "declaration" })
 vim.keymap.set({ "n", "v" }, "<leader>li", vim.lsp.buf.implementation, { desc = "implementation" })
-vim.keymap.set({ "n", "v" }, "<leader>lrn", vim.lsp.buf.rename, { desc = "rename" })
-vim.keymap.set({ "n", "v" }, "<leader>lrs", "<cmd>LspRestart<CR>", { desc = "restart" })
-vim.keymap.set({ "n", "v" }, "<leader>lf", function()
-	vim.lsp.buf.format({ async = true })
-end, { desc = "format" })
+vim.keymap.set({ "n", "v" }, "<leader>lr", vim.lsp.buf.rename, { desc = "rename" })
+vim.keymap.set({ "n", "v" }, "<leader>le", vim.lsp.buf.references, { desc = "references" })
+vim.keymap.set({ "n", "v" }, "<leader>ls", "<cmd>LspRestart<CR>", { desc = "restart" })
+vim.keymap.set({ "n", "v" }, "<leader>lf", function() vim.lsp.buf.format({ async = true }) end, { desc = "format" })
 
 -- Completion
 -- mappings inside completions.lua
@@ -67,17 +66,17 @@ vim.keymap.set({ "n", "v" }, "<leader>cs", "<cmd>Copilot status<CR>", { desc = "
 
 -- Workspace
 local function openWorkspace()
-	vim.cmd("Neotree filesystem left show reveal")
-	vim.defer_fn(function()
-		vim.cmd("AerialOpen! left")
-	end, 100)
+  vim.cmd("Neotree filesystem left show reveal")
+  vim.defer_fn(function()
+    vim.cmd("AerialOpen! left")
+  end, 100)
 end
 
 local function closeWorkspace()
-	vim.cmd("Neotree close")
-	vim.defer_fn(function()
-		vim.cmd("AerialCloseAll")
-	end, 100)
+  vim.cmd("Neotree close")
+  vim.defer_fn(function()
+    vim.cmd("AerialCloseAll")
+  end, 100)
 end
 
 vim.keymap.set({ "n", "v" }, "<leader>zz", openWorkspace, { desc = "workspace open" })
