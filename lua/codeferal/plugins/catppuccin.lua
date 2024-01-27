e224b51f177b2c24a34d3fb5aa6e81f9c2981f71
codeferal <codeferal@protonmail.com>
2024-01-27 17:36:55 +0200

    minor

--- lua/codeferal/plugins/catppuccin.lua
+++ lua/codeferal/plugins/catppuccin.lua
@@ -3,18 +3,20 @@ return {
 	name = "catppuccin",
 	priority = 1000,
 	config = function()
-		-- require("catppuccin").setup({
-		-- 	integrations = {
-		-- 		aerial = true,
-		-- 		dashboard = true,
-		-- 		leap = true,
-		-- 		mason = true,
-		-- 		neotree = true,
-		-- 		notify = true,
-		-- 		lsp_trouble = true,
-		-- 		which_key = true,
-		-- 	},
-		-- })
+		require("catppuccin").setup({
+			transparent_background = true,
+			-- integrations = {
+			-- 	aerial = true,
+			-- 	dashboard = true,
+			-- 	leap = true,
+			-- 	mason = true,
+			-- 	neotree = true,
+			-- 	notify = true,
+			-- 	lsp_trouble = true,
+			-- 	which_key = true,
+			-- },
+		})
+
 		vim.cmd.colorscheme("catppuccin")
 	end,
 }
