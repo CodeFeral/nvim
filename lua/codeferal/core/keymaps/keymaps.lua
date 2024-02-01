68f9bd2a663fab6676da7239d464df7f698e69b4
codeferal <codeferal@protonmail.com>
2024-02-01 14:11:18 +0200

    minor keymaps

--- lua/codeferal/core/keymaps/keymaps.lua
+++ lua/codeferal/core/keymaps/keymaps.lua
@@ -1,9 +1,9 @@
 -- set leader key to space
 vim.g.mapleader = " "
 
--- use jk to exit insert mode
-vim.keymap.set("i", "jk", "<ESC>", { desc = "exit insert mode with jk" })
-vim.keymap.set("i", "kj", "<ESC>", { desc = "exit insert mode with kj" })
+-- map jk and kj to escape
+vim.keymap.set("i", "jk", "<ESC>", { desc = "escape" })
+vim.keymap.set("i", "kj", "<ESC>", { desc = "escape" })
 
 -- Swap Ctrl-i and Ctrl-o
 vim.api.nvim_set_keymap("n", "<C-o>", "<C-i>", { noremap = true, silent = true })
