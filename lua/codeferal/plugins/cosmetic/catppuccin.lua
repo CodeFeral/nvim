27723542b2fcdbf579d049f914d273ec27ce1081
codeferal <codeferal@protonmail.com>
2024-02-12 16:56:41 +0200

    catpuccin updated integrations

--- lua/codeferal/plugins/cosmetic/catppuccin.lua
+++ lua/codeferal/plugins/cosmetic/catppuccin.lua
@@ -1,12 +1,50 @@
 return {
-	"catppuccin/nvim",
-	name = "catppuccin",
-	priority = 1000,
-	config = function()
-		require("catppuccin").setup({
-			transparent_background = true,
-		})
+  "catppuccin/nvim",
+  name = "catppuccin",
+  priority = 1000,
+  config = function()
+    require("catppuccin").setup({
+      transparent_background = true,
 
-		vim.cmd.colorscheme("catppuccin")
-	end,
+      integrations = {
+        dashboard = true,
+        leap = true,
+        mason = false,
+        neotree = false,
+        cmp = true,
+        notify = false,
+        treesitter = true,
+        lsp_trouble = false,
+        which_key = false,
+
+        -- telescope
+        telescope = {
+          enabled = true,
+          -- style = "nvchad"
+        },
+
+        -- nvim-lspconfig
+        native_lsp = {
+          enabled = true,
+          virtual_text = {
+            errors = { "italic" },
+            hints = { "italic" },
+            warnings = { "italic" },
+            information = { "italic" },
+          },
+          underlines = {
+            errors = { "underline" },
+            hints = { "underline" },
+            warnings = { "underline" },
+            information = { "underline" },
+          },
+          inlay_hints = {
+            background = true,
+          },
+        },
+      },
+    })
+
+    vim.cmd.colorscheme("catppuccin")
+  end,
 }
