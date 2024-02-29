c75c72ab6b61cb0caafca73681e0e940669ec32d
codeferal <codeferal@protonmail.com>
2024-02-29 17:26:13 +0200

    added word wrap toggle

--- lua/codeferal/core/keymaps/keymaps.lua
+++ lua/codeferal/core/keymaps/keymaps.lua
@@ -8,3 +8,6 @@ vim.keymap.set("i", "kj", "<ESC>", { desc = "escape" })
 -- Swap Ctrl-i and Ctrl-o
 vim.api.nvim_set_keymap("n", "<C-o>", "<C-i>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<C-i>", "<C-o>", { noremap = true, silent = true })
+
+-- Toggle Word Wrap
+vim.api.nvim_set_keymap({ "n", "v" }, "<leader>w", "<cmd>set wrap!<CR>", { desc = "Word Wrap" })
