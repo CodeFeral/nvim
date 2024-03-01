cf0b87a8bec6ac5e68d27f060ab623486e868c95
codeferal <codeferal@protonmail.com>
2024-03-02 01:23:30 +0200

    minor

--- lua/codeferal/plugins/obsidian.lua
+++ lua/codeferal/plugins/obsidian.lua
@@ -1,70 +1,75 @@
 return {
-	"epwalsh/obsidian.nvim",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-	},
+  "epwalsh/obsidian.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
 
-	version = "*",
-	lazy = true,
-	ft = "markdown",
+  version = "*",
+  lazy = true,
+  ft = "markdown",
 
-	config = function()
-		require("obsidian").setup({
-			dir = "~/core/vault",
+  config = function()
+    require("obsidian").setup({
+      dir = "~/core/vault",
 
-			notes_subdir = "nodes",
+      notes_subdir = "nodes",
 
-			daily_notes = {
-				folder = "nodes/dailies",
-			},
+      daily_notes = {
+        folder = "nodes/dailies",
+      },
 
-			mappings = {
-				["gf"] = {
-					action = function()
-						return require("obsidian").util.gf_passthrough()
-					end,
-					opts = { noremap = false, expr = true, buffer = true },
-				},
-			},
+      mappings = {
+        ["gf"] = {
+          action = function()
+            return require("obsidian").util.gf_passthrough()
+          end,
+          opts = { noremap = false, expr = true, buffer = true },
+        },
+      },
 
-			new_notes_location = "notes_subdir",
+      new_notes_location = "notes_subdir",
 
-			note_id_func = function(title)
-				return title
-			end,
+      note_id_func = function(title)
+        return title
+      end,
 
-			preferred_link_style = "wiki",
+      preferred_link_style = "wiki",
 
-			disable_frontmatter = true,
+      disable_frontmatter = true,
 
-			templates = {
-				subdir = "templates",
-				date_format = "%Y-%m-%d",
-				time_format = "%H:%M",
-				substitutions = {},
-			},
+      templates = {
+        subdir = "templates",
+        date_format = "%Y-%m-%d",
+        time_format = "%H:%M",
+        substitutions = {},
+      },
 
-			picker = {
-				name = "telescope.nvim",
-			},
+      ---@param url string
+      follow_url_func = function(url)
+        vim.fn.jobstart({ "xdg-open", url }) -- linux
+      end,
 
-			attachments = {
-				img_folder = "resources",
-				---@param client obsidian.Client
-				---@param path obsidian.Path
-				---@return string
-				img_text_func = function(client, path)
-					local link_path
-					local vault_relative_path = client:vault_relative_path(path)
-					if vault_relative_path ~= nil then
-						link_path = vault_relative_path
-					else
-						link_path = tostring(path)
-					end
-					local display_name = vim.fs.basename(link_path)
-					return string.format("![%s](%s)", display_name, link_path)
-				end,
-			},
-		})
-	end,
+      picker = {
+        name = "telescope.nvim",
+      },
+
+      attachments = {
+        img_folder = "resources",
+        ---@param client obsidian.Client
+        ---@param path obsidian.Path
+        ---@return string
+        img_text_func = function(client, path)
+          local link_path
+          local vault_relative_path = client:vault_relative_path(path)
+          if vault_relative_path ~= nil then
+            link_path = vault_relative_path
+          else
+            link_path = tostring(path)
+          end
+          local display_name = vim.fs.basename(link_path)
+          return string.format("![%s](%s)", display_name, link_path)
+        end,
+      },
+    })
+  end,
 }
